1a046bfe96aaa54ff44f555d69df95db33ba32cd
Riszkys <rizkysetiawann22@gmail.com>
2024-10-07 18:38:39 +0700

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        txtnama = New TextBox()
+        txtkategori = New TextBox()
+        txtharga = New TextBox()
+        txtstock = New TextBox()
+        DataGridView1 = New DataGridView()
+        btntambah = New Button()
+        btnedit = New Button()
+        btnhapus = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(36, 34)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(79, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Nama Barang"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(36, 93)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(91, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Kategori Barang"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(401, 44)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(39, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Harga"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(401, 100)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(36, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Stock"
+        ' 
+        ' txtnama
+        ' 
+        txtnama.Location = New Point(150, 38)
+        txtnama.Name = "txtnama"
+        txtnama.Size = New Size(100, 23)
+        txtnama.TabIndex = 4
+        ' 
+        ' txtkategori
+        ' 
+        txtkategori.Location = New Point(147, 90)
+        txtkategori.Name = "txtkategori"
+        txtkategori.Size = New Size(100, 23)
+        txtkategori.TabIndex = 5
+        ' 
+        ' txtharga
+        ' 
+        txtharga.Location = New Point(478, 44)
+        txtharga.Name = "txtharga"
+        txtharga.Size = New Size(100, 23)
+        txtharga.TabIndex = 6
+        ' 
+        ' txtstock
+        ' 
+        txtstock.Location = New Point(478, 97)
+        txtstock.Name = "txtstock"
+        txtstock.Size = New Size(100, 23)
+        txtstock.TabIndex = 7
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(71, 203)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(576, 192)
+        DataGridView1.TabIndex = 8
+        ' 
+        ' btntambah
+        ' 
+        btntambah.Location = New Point(49, 143)
+        btntambah.Name = "btntambah"
+        btntambah.Size = New Size(75, 23)
+        btntambah.TabIndex = 9
+        btntambah.Text = "Tamba Barang"
+        btntambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnedit
+        ' 
+        btnedit.Location = New Point(172, 143)
+        btnedit.Name = "btnedit"
+        btnedit.Size = New Size(75, 23)
+        btnedit.TabIndex = 10
+        btnedit.Text = "Update Barang"
+        btnedit.UseVisualStyleBackColor = True
+        ' 
+        ' btnhapus
+        ' 
+        btnhapus.Location = New Point(290, 143)
+        btnhapus.Name = "btnhapus"
+        btnhapus.Size = New Size(75, 23)
+        btnhapus.TabIndex = 11
+        btnhapus.Text = "Hapus"
+        btnhapus.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnhapus)
+        Controls.Add(btnedit)
+        Controls.Add(btntambah)
+        Controls.Add(DataGridView1)
+        Controls.Add(txtstock)
+        Controls.Add(txtharga)
+        Controls.Add(txtkategori)
+        Controls.Add(txtnama)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents txtkategori As TextBox
+    Friend WithEvents txtharga As TextBox
+    Friend WithEvents txtstock As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btntambah As Button
+    Friend WithEvents btnedit As Button
+    Friend WithEvents btnhapus As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    ' Connection string untuk menghubungkan ke MySQL
+    Dim connectionString As String = "server=localhost;user id=root;password=;database=kuisweek7"
+    Dim connection As New MySqlConnection(connectionString)
+
+    ' Method untuk membuka koneksi
+    Private Sub OpenConnection()
+        If connection.State = ConnectionState.Closed Then
+            connection.Open()
+        End If
+    End Sub
+
+    ' Method untuk menutup koneksi
+    Private Sub CloseConnection()
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+    End Sub
+
+    ' Method untuk load data barang ke DataGridView
+    Private Sub LoadData()
+        Try
+            OpenConnection()
+            Dim query As String = "SELECT * FROM inventarisbarang"
+            Dim cmd As New MySqlCommand(query, connection)
+            Dim adapter As New MySqlDataAdapter(cmd)
+            Dim table As New DataTable()
+
+            adapter.Fill(table)
+            DataGridView1.DataSource = table
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            CloseConnection()
+        End Try
+    End Sub
+
+    ' Method untuk menambah barang baru
+    Private Sub AddBarang()
+        Try
+            OpenConnection()
+            Dim query As String = "INSERT INTO inventarisbarang (nama, kategori, harga, stok) VALUES (@nama, @kategori, @harga, @stok)"
+            Dim cmd As New MySqlCommand(query, connection)
+
+            cmd.Parameters.AddWithValue("@nama", txtnama.Text)
+            cmd.Parameters.AddWithValue("@kategori", txtkategori.Text)
+            cmd.Parameters.AddWithValue("@harga", txtharga.Text)
+            cmd.Parameters.AddWithValue("@stok", txtstock.Text)
+
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Barang berhasil ditambahkan")
+            LoadData()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            CloseConnection()
+        End Try
+    End Sub
+    ' Method untuk menghapus barang (ID diambil dari DataGridView)
+    Private Sub DeleteBarang()
+        Try
+            If DataGridView1.SelectedRows.Count > 0 Then
+                Dim id As Integer = DataGridView1.SelectedRows(0).Cells("id").Value
+                OpenConnection()
+                Dim query As String = "DELETE FROM inventarisbarang WHERE id = @id"
+                Dim cmd As New MySqlCommand(query, connection)
+                cmd.Parameters.AddWithValue("@id", id)
+
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Barang berhasil dihapus")
+                LoadData()
+            Else
+                MessageBox.Show("Pilih barang yang ingin dihapus")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            CloseConnection()
+        End Try
+    End Sub
+
+    ' Method untuk mengedit barang (ID diambil dari DataGridView)
+    Private Sub UpdateBarang()
+        Try
+            If DataGridView1.SelectedRows.Count > 0 Then
+                Dim id As Integer = DataGridView1.SelectedRows(0).Cells("id").Value
+                OpenConnection()
+                Dim query As String = "UPDATE inventarisbarang SET nama=@nama, kategori=@kategori, harga=@harga, stok=@stok WHERE id=@id"
+                Dim cmd As New MySqlCommand(query, connection)
+
+                cmd.Parameters.AddWithValue("@id", id)
+                cmd.Parameters.AddWithValue("@nama", txtnama.Text)
+                cmd.Parameters.AddWithValue("@kategori", txtkategori.Text)
+                cmd.Parameters.AddWithValue("@harga", txtharga.Text)
+                cmd.Parameters.AddWithValue("@stok", txtstock.Text)
+
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Barang berhasil diperbarui")
+                LoadData()
+            Else
+                MessageBox.Show("Pilih barang yang ingin diedit")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            CloseConnection()
+        End Try
+    End Sub
+
+    ' Event handler untuk button tambah
+
+    ' Form Load untuk menampilkan data
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadData()
+    End Sub
+
+    Private Sub btntambah_Click(sender As Object, e As EventArgs) Handles btntambah.Click
+        AddBarang()
+    End Sub
+
+    Private Sub btnedit_Click(sender As Object, e As EventArgs) Handles btnedit.Click
+        UpdateBarang()
+    End Sub
+
+    Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles btnhapus.Click
+        DeleteBarang()
+    End Sub
+End Class
